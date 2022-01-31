dae54dfb829b52c4b5c83eb5ed49a8e53b0b2070
moniratna <moniratna.munshi123@gmail.com>
2022-01-31 17:29:36 +0530

    finished

---
+++ routes/users.js
@@ -0,0 +1,55 @@
+const { User, validate } = require('../models/user');
+const express = require('express');
+const router = express.Router();
+const nodemailer = require("nodemailer");
+
+router.post('/register', async (req, res) => {
+    const sendMail = (email)=>{
+        const Transport = nodemailer.createTransport({
+            service: "Gmail",
+            auth: {
+                user: "munai.munshi.moniratna@gmail.com",
+                pass: "hpivtpeuskewgcck"
+            }
+        });
+        var mailOptions;
+        let sender = "Moniratna";
+        mailOptions = {
+            from: sender,
+            to: email,
+            subject: "Email Confirmation",
+            html: "Welcome to our website. Thanks"
+        };
+        Transport.sendMail(mailOptions, function(error,response){
+            if(error){
+                console.log(error);
+            }else{
+                console.log("Message sent: " + response.message);
+            }
+        })
+    }
+    // First Validate The Request
+    const { error } = validate(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    // Check if this user already exisits
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+        return res.status(400).send('That user already exisits!');
+    } else {
+        // Insert the new user if they do not exist yet
+        user = new User({
+            FirstName: req.body.FirstName,
+            LastName: req.body.LastName,
+            email: req.body.email,
+            password: req.body.password
+        });
+        await user.save();
+        sendMail(req.body.email)
+        res.send(user);
+    }
+});
+
+module.exports = router;
